6ca95a0a9b49ff227a7f5cf4ff02154035945809
andersoncorreia <anderson-strife@outlook.com>
2020-10-11 19:00:48 -0300

    3D visualização

--- jogodamemoria.cpp
+++ jogodamemoria.cpp
@@ -17,8 +17,12 @@ JogoDaMemoria::~JogoDaMemoria() {}
 // Initialize OpenGL
 void JogoDaMemoria::initializeGL() {
     qglClearColor(Qt::white);
+    glClearDepth(10.0f);									// Depth Buffer Setup
+    glClearStencil(0);									// Stencil Buffer Setup
     glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
-    glDepthFunc(GL_LEQUAL);
+    glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+
 }
 
 // This is called when the OpenGL window is resized
@@ -32,36 +36,36 @@ void JogoDaMemoria::resizeGL(int width, int height) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity(); // Reset projection matrix
 
-    gluPerspective(45.0f, static_cast<GLfloat>(width)/height, 5.f, 250.0f); // Calculate aspect ratio
+    gluPerspective(5.0f, static_cast<GLfloat>(width)/height, 0.f, 250.0f); // Calculate aspect ratio
 
     // Especifica posição do observador e do alvo
-    gluLookAt(40.f,60.f,100.f, 0,0,0, 0,1,0);
+    gluLookAt(0.f,0.f,20.f, 0,0,0, 0,1,0);
 }
 
 // OpenGL painting code goes here
 void JogoDaMemoria::paintGL() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear screen and depth buffer
-    glLoadIdentity(); // Reset current modelview matrix
+    glMatrixMode(GL_MODELVIEW);
+
     for (int i = 0; i<8; i++) {
         if( i < 4 ){
-            DesenhaCarta(i == cartaSelecionada, -0.7+ 0.35*(i%4), 0.7, i%4+1);
+            DesenhaCarta(i == cartaSelecionada, -0.7+ 0.38*(i%4), 0.8, i%4+1);
         }
         else {
-            //glRotatef(15, 1, 0, 0);
-            DesenhaCarta(i == cartaSelecionada, -0.7 + 0.35*(i%4) , 0, i%4+1);
-            //glRotatef(-15, 1, 0, 0);
+            DesenhaCarta(i == cartaSelecionada, -0.7 + 0.38*(i%4) , -0.1, i%4+1);
         }
     }
 }
 
 void JogoDaMemoria::DesenhaCarta(bool selecionado, float x_init, float y_init, int figura){
+    //glRotatef(15, 1, 0, 0);
     glColor3f(0.7, 0.7, 0.7);//trocando cor para cinza
 
     glBegin(GL_QUADS);
-        glVertex3f(x_init, y_init, 0.1);
-        glVertex3f(x_carta + x_init, y_init, 0.1);
-        glVertex3f(x_carta + x_init, y_carta + y_init, 0.1);
-        glVertex3f(x_init, y_carta + y_init, 0.1);
+        glVertex3f(x_init, y_init, -10);
+        glVertex3f(x_carta + x_init, y_init, -10);
+        glVertex3f(x_carta + x_init, y_carta + y_init, -10);
+        glVertex3f(x_init, y_carta + y_init, -10);
 
         glVertex3f(x_init, y_init, 0);
         glVertex3f(x_carta + x_init, y_init, 0);
@@ -91,11 +95,16 @@ void JogoDaMemoria::DesenhaCarta(bool selecionado, float x_init, float y_init, i
 
     glLineWidth(5.0f);
     glBegin(GL_LINE_LOOP);//desenhando a borda da carta
-        glVertex3f(x_init, y_init, 1);
-        glVertex3f(x_carta + x_init, y_init, 1);
-        glVertex3f(x_carta + x_init, y_carta + y_init, 1);
-        glVertex3f(x_init, y_carta + y_init, 1);
+        glVertex3f(x_init, y_init, 0.1);
+        glVertex3f(x_carta + x_init, y_init, 0.1);
+        glVertex3f(x_carta + x_init, y_carta + y_init, 0.1);
+        glVertex3f(x_init, y_carta + y_init, 0.1);
+        glVertex3f(x_init, y_init, 0.01);
+        glVertex3f(x_carta + x_init, y_init, 0.01);
+        glVertex3f(x_carta + x_init, y_carta + y_init, 0.01);
+        glVertex3f(x_init, y_carta + y_init, 0.01);
     glEnd();
+    //glRotatef(-15, 1, 0, 0);
 }
 
 void JogoDaMemoria::DesenhaCubo(float x_init, float y_init){
@@ -107,10 +116,10 @@ void JogoDaMemoria::DesenhaCubo(float x_init, float y_init){
 
     glColor3f(1, 0, 0);//trocando cor para vermelho
     glBegin(GL_QUADS);
-        glVertex3f(x_init, y_init, 0);
-        glVertex3f(x_end, y_init, 0);
-        glVertex3f(x_end, y_end, 0);
-        glVertex3f(x_init, y_end, 0);
+        glVertex3f(x_init, y_init, 0.01);
+        glVertex3f(x_end, y_init, 0.01);
+        glVertex3f(x_end, y_end, 0.01);
+        glVertex3f(x_init, y_end, 0.01);
     glEnd();
 }
 
@@ -123,9 +132,9 @@ void JogoDaMemoria::DesenhaTriangulo(float x_init, float y_init){
 
     glColor3f(0, 0, 1);//trocando cor para azul
     glBegin(GL_POLYGON);
-        glVertex3f(x_end/2+x_init/2, y_init, 0);
-        glVertex3f(x_end, y_end, 0);
-        glVertex3f(x_init, y_end, 0);
+        glVertex3f(x_end/2+x_init/2, y_init, 0.01);
+        glVertex3f(x_end, y_end, 0.01);
+        glVertex3f(x_init, y_end, 0.01);
     glEnd();
 }
 void JogoDaMemoria::DesenhaIgual(float x_init, float y_init){
@@ -137,15 +146,15 @@ void JogoDaMemoria::DesenhaIgual(float x_init, float y_init){
 
     glColor3f(0, 1, 0.3);//trocando cor para azul
     glBegin(GL_QUADS);
-        glVertex3f(x_init, y_init, 0);
-        glVertex3f(x_end, y_init, 0);
-        glVertex3f(x_end, y_init + 1.5*y_carta/8, 0);
-        glVertex3f(x_init, y_init + 1.5*y_carta/8, 0);
+        glVertex3f(x_init, y_init, 0.01);
+        glVertex3f(x_end, y_init, 0.01);
+        glVertex3f(x_end, y_init + 1.5*y_carta/8, 0.01);
+        glVertex3f(x_init, y_init + 1.5*y_carta/8, 0.01);
 
-        glVertex3f(x_init, y_init + 2.5*y_carta/8, 0);
-        glVertex3f(x_end, y_init + 2.5*y_carta/8, 0);
-        glVertex3f(x_end, y_end, 0);
-        glVertex3f(x_init, y_end, 0);
+        glVertex3f(x_init, y_init + 2.5*y_carta/8, 0.01);
+        glVertex3f(x_end, y_init + 2.5*y_carta/8, 0.01);
+        glVertex3f(x_end, y_end, 0.01);
+        glVertex3f(x_init, y_end, 0.01);
     glEnd();
 }
 void JogoDaMemoria::DesenhaLosangulo(float x_init, float y_init){
@@ -157,10 +166,10 @@ void JogoDaMemoria::DesenhaLosangulo(float x_init, float y_init){
 
     glColor3f(0.6, 0, 0.8);//trocando cor para azul
     glBegin(GL_QUADS);
-    glVertex3f(x_init, (y_init + y_end)/2, 0);
-    glVertex3f((x_init + x_end)/2, y_init, 0);
-    glVertex3f(x_end, (y_init + y_end)/2, 0);
-    glVertex3f((x_init + x_end)/2, y_end, 0);
+    glVertex3f(x_init, (y_init + y_end)/2, 0.01);
+    glVertex3f((x_init + x_end)/2, y_init, 0.01);
+    glVertex3f(x_end, (y_init + y_end)/2, 0.01);
+    glVertex3f((x_init + x_end)/2, y_end, 0.01);
     glEnd();
 }
 
